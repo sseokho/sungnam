d8d898bf8595f67d2b2acbd4b96f77f132698ca0
pub <tjrgh538@naver.com>
2022-11-07 17:36:02 +0900

    1

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,13 +1,57 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper--content1.mySwiper", {
     slidesPerView: 5,
-    spaceBetween: 17,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    spaceBetween: 20,
 
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".con1 .swiper-button-next",
+        prevEl: ".con1 .swiper-button-prev",
       },
   });
+
+  var swiper2 = new Swiper(".mySwiper--content2.mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 20,
+
+    navigation: {
+        nextEl: ".con2 .swiper-button-next",
+        prevEl: ".con2 .swiper-button-prev",
+      },
+  });
+
+  
+
+    
+    $(".mySwiper--content1 .state_link").hover(
+        
+        function(){   
+            
+          let idx = $(".mySwiper--content1 .slide-content .state_link").index(this);
+            $(".mySwiper--content1 .slide-content").eq(idx).addClass("is-active");
+            
+        },
+        function(){
+            $(".mySwiper--content1 .slide-content").removeClass("is-active");
+        }
+
+        
+    );
+
+    $(".mySwiper--content2 .state_link").hover(
+        
+      function(){   
+          
+        let idx = $(".mySwiper--content2 .slide-content .state_link").index(this);
+          $(".mySwiper--content2 .slide-content").eq(idx).addClass("is-active");
+          
+      },
+      function(){
+          $(".mySwiper--content2 .slide-content").removeClass("is-active");
+      }
+
+      
+  );
+
+
+
+
+
